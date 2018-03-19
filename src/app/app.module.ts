566b138bb8d4c256b0fcfae4e9a8750adbf20fb6
DIR\o.alvarado.quiroga <o.alvarado.quiroga@accenture.com>
2018-03-19 18:32:59 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { VideojuegosComponent } from "./videojuego/videojuegos.components";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideojuegosComponent
   ],
   imports: [
     BrowserModule
