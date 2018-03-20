a05339a07f12bb58bc2dc27f1e13f60dece968d1
DIR\o.alvarado.quiroga <o.alvarado.quiroga@accenture.com>
2018-03-20 15:28:26 -0500

    component_videojuegos

--- src/app/videojuego/videojuegos.components.ts
+++ src/app/videojuego/videojuegos.components.ts
@@ -2,13 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-video-juegos',
-    template: `
-        <h2>Componente de {{nombre}}</h2>
-        <h3>El mejor juego es: {{mejor_juego}}</h3>
-    `
+    templateUrl: './videojuegos.component.html'
 })
 export class VideojuegosComponent {
-    public nombre = 'Videojuegos 2018';
-    public mejor_juego = 'GTA 5';
+    public nombre: string;
+    public mejor_juego: string;
+    public mejor_juego_retro: string;
+    public mostrar_retro: boolean;
+
+    public year: number;
+
+    public videojuegos: Array<any>;
+    constructor() {
+      this.nombre = 'Videojuegos 2018';
+      this.mejor_juego = 'GTA 5';
+      this.mejor_juego_retro = 'Super Mario 64';
+      this.mostrar_retro = true;
+      this.year = 2018;
+      this.videojuegos = [
+        'Los Simpsons hit and run',
+        'Assasins Creed',
+        'GTA 5',
+        'Call of Duty',
+        'Tekken 7',
+        12345
+      ];
+    }
 }
 
